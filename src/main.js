b75df2f26536aa94dc2744d2d17a266d36929a77
root <root@LAPTOP-ID12V90L>
2025-03-21 16:40:43 -0700

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+window.increment = function() {
+  count += 1;
+  countEl.textContent = count;
+};
+
+window.save = function() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+};
